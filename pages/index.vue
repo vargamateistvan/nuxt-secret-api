841c48ca78add7d763b1adfba43c3d7cb0192da5
Máté Varga <mate.varga@talentuno.com>
2021-06-17 15:00:00 +0200

    Add some design

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,63 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        Secrets
-      </h1>
-      Secret:
-      <input v-model="secretText" type="text">
-      Expire after view
-      <input v-model="expireAfterViews" type="number" min="1">
-      Expire after
-      <input v-model="expireAfter" type="number" min="0">
-      <button @click="createSecret">
-        Create Secret
-      </button>
-      <button @click="getAllSecret">
-        Refresh list
-      </button>
-        <div
-          v-for="(currentSecret, index) in secrets"
-          :key="index"
-          class="secret"
-        >
-        <span>Hash: {{currentSecret.hash}}</span>
-        <span>Expire at: {{currentSecret.expiresAt > 0 ? new Date(currentSecret.expiresAt) : 'No time limit'}}</span>
-        <span>Remaing view: {{currentSecret.remainingViews}}</span>
-          <button @click="getSecret(currentSecret.hash)">
-            Check Secret
-          </button>
-        </div>
-    </div>
+  <div>
+    <v-form>
+      <v-container>
+        <v-row no-gutters>
+          <v-col>
+            Secret: <v-text-field v-model="secretText" type="text" label="Secret text" />
+          </v-col>
+          <v-col>
+            Expire after view <v-text-field v-model="expireAfterViews" required type="number" min="1" />
+          </v-col>
+          <v-col>
+            Expire after <v-text-field v-model="expireAfter" required type="number" min="0" />
+          </v-col>
+          <v-col>
+            <v-btn @click="createSecret">
+              Create Secret
+            </v-btn>
+          </v-col>
+          <v-col>
+            <v-btn @click="getAllSecret">
+              Refresh list
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
+    <v-container>
+      <v-simple-table>
+        <template #default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Hash
+              </th>
+              <th class="text-left">
+                Expire at
+              </th>
+              <th class="text-left">
+                Remaing view
+              </th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(currentSecret, index) in secrets" :key="index">
+              <td>{{ currentSecret.hash }}</td>
+              <td>{{ currentSecret.expiresAt > 0 ? new Date(currentSecret.expiresAt) : 'No time limit' }}</td>
+              <td>{{ currentSecret.remainingViews }}</td>
+              <td>
+                <v-btn @click="getSecret(currentSecret.hash)">
+                  Check Secret
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-container>
   </div>
 </template>
 
@@ -41,10 +70,11 @@ export default {
       secretText: '',
       expireAfterViews: 1,
       expireAfter: 0,
-      secrets: []
+      secrets: [],
+      valid: false
     }
   },
-  created() {
+  created () {
     this.getAllSecret()
   },
   methods: {
@@ -54,13 +84,13 @@ export default {
         expireAfterViews: this.expireAfterViews,
         expireAfter: this.expireAfter
       }
+
       await post('http://localhost:3000/api/secret', request)
       this.getAllSecret()
     },
     async getSecret (hash) {
       await get(`http://localhost:3000/api/secret/${hash}`)
       this.getAllSecret()
-
     },
     async getAllSecret () {
       this.secrets = await get('http://localhost:3000/api/secrets')
@@ -68,35 +98,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
--- utils/request.js
+++ utils/request.js
@@ -1,30 +1,30 @@
 export const get = async (url) => {
-    const response = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-    });
-    return await response.json();
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  })
+  return await response.json()
 }
 
 export const post = async (url, request) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(request),
-    });
-    return await response.json();
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(request)
+  })
+  return await response.json()
 }
